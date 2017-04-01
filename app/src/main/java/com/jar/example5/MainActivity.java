3b9745636800e27bc7663163cf0bc5964543325f
j-alberto <jalberto.roj@gmail.com>
2017-04-01 13:38:08 -0600

    shared preferences

--- app/src/main/java/com/jar/example5/MainActivity.java
+++ app/src/main/java/com/jar/example5/MainActivity.java
@@ -1,10 +1,12 @@
 package com.jar.example5;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.FileNotFoundException;
@@ -13,7 +15,12 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String MIS_DATOS = "Mis datos";
+    public static final String NAME = "name";
+    public static final String EMAIL = "email";
     EditText etName;
+    EditText etEmail;
+    TextView tvPreference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         etName = (EditText) findViewById(R.id.etName);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        tvPreference = (TextView) findViewById(R.id.tvPreference);
     }
 
     public void makeFile(View view) {
@@ -37,5 +46,30 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
             Toast.makeText(this,"No se pudo leer el archivo",Toast.LENGTH_SHORT).show();
         }
+    }
+
+    public void showPreference(View view) {
+        SharedPreferences prefs = getSharedPreferences(MIS_DATOS, Context.MODE_PRIVATE);
+        String name = prefs.getString(NAME,"Sin nombre");
+        String email = prefs.getString(EMAIL,"Sin email");
+
+        tvPreference.setText(String.format("Name:\t%s \nEmail:\t%s",name,email));
+    }
+
+    public void savePreference(View view) {
+        SharedPreferences prefs = getSharedPreferences(MIS_DATOS, Context.MODE_PRIVATE);
+        SharedPreferences.Editor prefEditor = prefs.edit();
+
+        String name = etName.getText().toString();
+        String email = etEmail.getText().toString();
+
+        prefEditor.putString(NAME, name);
+        prefEditor.putString(EMAIL, email);
+
+        prefEditor.commit();
+
+        Toast.makeText(this, "Preferencias compartidas guardadas",Toast.LENGTH_LONG).show();
+
+
     }
 }
